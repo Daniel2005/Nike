fcaed58647662e1e216b568ddb9a36fb5db3c599
Daniel2005 <danielefremov50@gmail.com>
2023-12-02 16:21:11 +0200

    Change

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -15,7 +15,7 @@ const ShoeCard = ({imgURL, changeBigShoeImage, bigShoeImg}) => {
     `}
       onClick={handleClick}
     >
-      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:h-40 w-40 rounded-xl max-sm:p-4 max-sm:w-[140px] max-xs:w-[100px]">
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:h-40 w-40 rounded-xl max-xs:w-[100px] max-sm:p-4 max-sm:w-[140px] ">
         <img
           src={imgURL.thumbnail}
           alt="shoe collection"
